fcfd1dd8ce04bf647db689079400dcb54d856d84
Claas Diederichs <claas.diederichs@cd-its.de>
2022-12-11 17:27:56 +0100

    include 'duration' format

--- CdIts.Caffoa.Cli/Extensions.cs
+++ CdIts.Caffoa.Cli/Extensions.cs
@@ -74,6 +74,7 @@ public static class Extensions
             "string" when format is "date-time" => $"DateTime{suffix}",
             "string" when format is "date" => $"DateOnly{suffix}",
             "string" when format is "time" => $"TimeOnly{suffix}",
+            "string" when format is "duration" => $"TimeSpan{suffix}",
             "integer" when format == "int64" => $"long{suffix}",
             "integer" when format == "uint64" => $"ulong{suffix}",
             "integer" when format == "uint32" => $"uint{suffix}",
