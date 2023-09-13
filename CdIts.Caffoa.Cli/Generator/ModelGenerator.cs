e6471096a1fae1c3f05a1ebc287c9297c9ef9650
Bernhard Klefer <bernhard.klefer@beenera.de>
2023-09-13 19:29:34 +0200

    Ugly workaround to target System.Text.Json for .NET 6 and .NET 7

--- CdIts.Caffoa.Cli/Generator/ModelGenerator.cs
+++ CdIts.Caffoa.Cli/Generator/ModelGenerator.cs
@@ -48,7 +48,8 @@ public class ModelGenerator
         parameters["NAMESPACE"] = _service.Model!.Namespace;
         parameters["NAME"] = item.ClassName;
         parameters["DESCRIPTION"] = formatter.Description;
-        parameters["ATTRIBUTES"] = _config.Flavor == CaffoaConfig.GenerationFlavor.SystemTextJson ? string.Join("\n    ", item.Interface!.Children.Select(c => $"[JsonDerivedType(typeof({c}))]")) : string.Empty;
+        parameters["ATTRIBUTES"] = (_config.Flavor == CaffoaConfig.GenerationFlavor.SystemTextJson && _service.Controller !=null && _service.Function == null ) 
+            ? string.Join("\n    ", item.Interface!.Children.Select(c => $"[JsonDerivedType(typeof({c}))]")) : string.Empty; //Needed to serialize interfaces. supported starting .NET 7. So only use when generating controllers.
         parameters["TYPE"] = item.Interface?.Discriminator?.ToObjectName() ?? "";
         parameters["IMPORTS"] = PropertyFormatter.Imports(_config.Flavor);
         var formatted = file.FormatDict(parameters);
