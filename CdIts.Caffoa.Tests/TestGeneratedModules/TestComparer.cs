2b5d97f276b58699cd2abbcd451bbbdaa49e360d
Claas Diederichs <claas.diederichs@cd-its.de>
2024-10-30 22:34:46 +0100

    add additional tests

--- CdIts.Caffoa.Tests/TestGeneratedModules/TestComparer.cs
+++ CdIts.Caffoa.Tests/TestGeneratedModules/TestComparer.cs
@@ -22,4 +22,18 @@ public class TestComparer
         user.Equals(user2).Should().Be(false);
         user2.Equals(user).Should().Be(false);
     }
+    [Test]
+    public void EqualsShouldConsiderNullSequencesEqual()
+    {
+        var user = new User()
+        {
+            Emails = null
+        };
+        var user2 = new User(user)
+        {
+            Emails = null
+        };
+        user.Equals(user2).Should().Be(true);
+        user2.Equals(user).Should().Be(true);
+    }
 }
